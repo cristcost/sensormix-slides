0e6744c45e8d9451f47c90f671abb5c09e8fdad6
cristcost <cristiano.costantini@gmail.com>
2013-12-10 00:20:32 +0100

    first 3 slides

--- slide_config.js
+++ slide_config.js
@@ -2,7 +2,7 @@ var SLIDE_CONFIG = {
   // Slide settings
   settings: {
     title: 'Sviluppare su OSGI<br/>con Camel e GWT',
-    subtitle: 'Introduzione',
+    subtitle: 'Aperitivo tecnologico del GDG Firenze<br/>http://www.gdg-firenze.info/',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
